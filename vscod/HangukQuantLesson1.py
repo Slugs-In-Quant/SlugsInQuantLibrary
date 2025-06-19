a9fca286ed58abe302e93f35a04e3a979f1cb6c6
Gary John <garrukhunt@gmail.com>
2025-06-19 14:33:14 -0700

    HangukQuantLesson1.py

--- vscod/HangukQuantLesson1.py
+++ vscod/HangukQuantLesson1.py
@@ -11,6 +11,9 @@ def get_sp_500_ticker():
     df = pd.read_html(str(table))
     #takes the df and only outputs symbol, if the table on Wiki used some other word(ticker), we would write ticker here.
     tickers_list = list(df[0].Symbol)
+    #We do this because of those pesky pesky class B stocks that wikipedia uses a "." and yfinance uses a "-" for
+    #so we replace every period with a dash for each ticker in the tickers_list!
+    tickers_list = [item.replace(".", "-") for item in tickers_list]
     return tickers_list
     #print(df[0].to_json(orient='records'))
 ticker = get_sp_500_ticker()
